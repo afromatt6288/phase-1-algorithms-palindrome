b961f01471643a8b74cce726510af44594ecdc6c
Matthew Clark <afromatt6288@hotmail.com>
2023-02-16 16:29:23 -0700

    Lab Complete

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  wordArray = []
+  wordArray = word.split("")
+  console.log(wordArray)
+  backwardWordArray = []
+  backwardWordArray = [...wordArray].reverse()
+  console.log(backwardWordArray)
+  backwardWord = backwardWordArray.join("")
+  console.log(backwardWord)
+  if (word === backwardWord) {
+    return true
+  } else {
+    return false
+  }
+
 }
+console.log(isPalindrome(""))
 
 /* 
   Add your pseudocode here
+  word to array
+  split word into characters
+  reverse word array
+  rejoin reversed word into backward word
+  compare word and backward word
+  return a true or false result
 */
 
 /*
   Add written explanation of your solution here
+  I create a word array.
+  I used split to make that array contain the word with each character as it's own element.
+  I created a backwardWord array
+  I used reverse and the spread operator to populate that with the words letters in reverse order
+  I used join to make the backwarWordArray into backwardWord
+  I compared the word and backwardWord, and set it to give a true or a false using an if else statement
 */
 
 // You can run `node index.js` to view these console logs
